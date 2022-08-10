0c9dd6697f4b22d85df54b48c2bea2badc4c779d
marsmaantje[laptop] <marsmaantje@planet.nl>
2022-08-10 16:37:42 +0200

    Initial commit

---
+++ RP3 Interface/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace RP3_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.FindMachine = new System.Windows.Forms.Button();
+            this.PortsList = new System.Windows.Forms.CheckedListBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            chartArea1.AxisY.Maximum = 300D;
+            chartArea1.Name = "ChartArea1";
+            chartArea2.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(214, 51);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderWidth = 22;
+            series1.Name = "Series1";
+            series2.BorderWidth = 5;
+            series2.ChartArea = "ChartArea2";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1027, 619);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // FindMachine
+            // 
+            this.FindMachine.Location = new System.Drawing.Point(56, 67);
+            this.FindMachine.Name = "FindMachine";
+            this.FindMachine.Size = new System.Drawing.Size(114, 23);
+            this.FindMachine.TabIndex = 3;
+            this.FindMachine.Text = "Find machine";
+            this.FindMachine.UseVisualStyleBackColor = true;
+            this.FindMachine.Click += new System.EventHandler(this.FindMachine_Click);
+            // 
+            // PortsList
+            // 
+            this.PortsList.CheckOnClick = true;
+            this.PortsList.FormattingEnabled = true;
+            this.PortsList.HorizontalScrollbar = true;
+            this.PortsList.Location = new System.Drawing.Point(56, 156);
+            this.PortsList.Name = "PortsList";
+            this.PortsList.Size = new System.Drawing.Size(120, 94);
+            this.PortsList.TabIndex = 5;
+            this.PortsList.SelectedIndexChanged += new System.EventHandler(this.PortsList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 865);
+            this.Controls.Add(this.PortsList);
+            this.Controls.Add(this.FindMachine);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button FindMachine;
+        private System.Windows.Forms.CheckedListBox PortsList;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ RP3 Interface/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+
+
+namespace RP3_Interface
+{
+    public partial class Form1 : Form
+    {
+        public static bool formOpen = true;
+        double lastRps = 0;
+        double change = 0;
+
+
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            Console.WriteLine("hello world");
+
+            Timer updateClock = new Timer();
+            //updateClock.Interval = 1000;
+            updateClock.Interval = 1000 / 60;
+            updateClock.Tick += Update;
+            updateClock.Start();
+
+            this.FormClosing += formClosing;
+            serialPort1.DataReceived += dataReceived;
+
+        }
+
+        void formClosing(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            formOpen = false;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            //add a point relative to mouse y position
+            chart1.Series.First()?.Points.AddY(MousePosition.Y);
+            if (chart1.Series.First()?.Points.Count > 6)
+            {
+                //remove first index
+                chart1.Series.First()?.Points.RemoveAt(0);
+            }
+        }
+
+        private void FindMachine_Click(object sender, EventArgs e)
+        {
+            string selectedName = "";
+            if (PortsList.CheckedItems.Count > 0)
+                selectedName = (string)PortsList.CheckedItems[0];
+
+            //add all port names to PortsList as selectable checkboxes
+            PortsList.Items.Clear();
+            string[] names = SerialPort.GetPortNames();
+            foreach (string s in names)
+            {
+                int i = PortsList.Items.Add(s);
+
+                if (s.Equals(selectedName))
+                {
+                    Console.WriteLine("setting index to " + i);
+                    PortsList.SetItemChecked(i, true);
+                }
+            }
+        }
+
+        private void PortsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+            serialPort1.PortName = (string)PortsList.CheckedItems[0];
+            serialPort1.Open();
+            Console.WriteLine("current open port: " + serialPort1.PortName + ": " + serialPort1.IsOpen);
+        }
+
+        private void dataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!serialPort1.IsOpen || serialPort1.BytesToRead < 2)
+                return;
+            Console.WriteLine(serialPort1.BytesToRead);
+
+            byte[] input = new byte[serialPort1.BytesToRead];
+            int valueCount = serialPort1.Read(input, 0, input.Length);
+
+            int i = 0;
+            while (i < valueCount && i < input.Length)
+            {
+                int value = ((input[i] & 0xff) << 8 | (input[i + 1] & 0xff)) & 0xffff;
+                double wheelValue = value / 750000.0;
+                double rps = 1 / (wheelValue * 4);
+
+                /*
+                chart1.Series[0]?.Points.AddY(rps);
+                if (chart1.Series[0]?.Points.Count > 500)
+                {
+                    //remove first index
+                    chart1.Series[0]?.Points.RemoveAt(0);
+                }
+
+                double maxChartValue = 0;
+                foreach (var point in chart1.Series[0]?.Points)
+                {
+                    if (point.YValues[0] > maxChartValue)
+                        maxChartValue = point.YValues[0];
+                }
+
+                chart1.ChartAreas[0].AxisY.Maximum = maxChartValue * 1.1;
+                */
+                //addPoint(0, rps, 500, true);
+
+                change = rps - lastRps;
+                lastRps = rps;
+                //addPoint(1, change, 500, true, true);
+
+                i += 4;
+            }
+        }
+
+        /// <summary>
+        /// Update method, gets run on a loop while the window is open.
+        /// </summary>
+        private void Update(object sender, EventArgs e)
+        {
+            addPoint(0, lastRps, 300, true, false);
+            addPoint(1, change, 300, true, true);
+        }
+
+        private void addPoint(int chartIndex, double value, int pointCount, bool scaleMax = false, bool scaleMin = false)
+        {
+            chart1.Series[chartIndex]?.Points.AddY(value);
+            if (chart1.Series[chartIndex]?.Points.Count > pointCount)
+            {
+                //remove first index
+                chart1.Series[chartIndex]?.Points.RemoveAt(0);
+            }
+
+            if (scaleMax)
+            {
+                double maxChartValue = 0;
+                foreach (var point in chart1.Series[chartIndex]?.Points)
+                {
+                    if (point.YValues[0] > maxChartValue)
+                        maxChartValue = point.YValues[0];
+                }
+                chart1.ChartAreas[chartIndex].AxisY.Maximum = maxChartValue + (Math.Abs(maxChartValue) * 0.1) + 0.1;
+            }
+
+            if (scaleMin)
+            {
+                double minChartValue = 0;
+                foreach (var point in chart1.Series[chartIndex]?.Points)
+                {
+                    if (point.YValues[0] < minChartValue)
+                        minChartValue = point.YValues[0];
+                }
+                chart1.ChartAreas[chartIndex].AxisY.Minimum = minChartValue - (Math.Abs(minChartValue) * 0.1) - 0.1;
+            }
+        }
+    }
+}
